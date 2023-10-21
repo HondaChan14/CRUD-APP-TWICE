4fed1b205360c01eca221a24abd7da35098205af
HondaChan14 <92832561+HondaChan14@users.noreply.github.com>
2023-10-21 00:13:40 -0400

    clown shit 8

--- server.js
+++ server.js
@@ -1,82 +1,86 @@
-const express = require('express')
-const app = express()
-const MongoClient = require('mongodb').MongoClient
-const PORT = 2121
-require('dotenv').config()
+const express = require('express');
+const app = express();
+const { MongoClient } = require('mongodb');
+const PORT = 2121;
+require('dotenv').config();
 
 let db,
     dbConnectionStr = process.env.DB_STRING,
-    dbName = 'star-wars-quotes'
-var dbCollection
+    dbName = 'star-wars-quotes',
+    collectionName = 'rappers';
 
-    MongoClient.connect(dbConnectionStr)
-    .then(client => {
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Wrap the connection code in an async function to use await
+async function connectToDatabase() {
+    try {
+        const client = await MongoClient.connect(dbConnectionStr);
         console.log(`Connected to ${dbName} Database`);
         db = client.db(dbName);
-        dbCollection = db.collection('rappers')
-    })
-    .catch(error => {
+        collection = db.collection(collectionName);
+    } catch (error) {
         console.error('Error connecting to the database:', error);
-    });
-    
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
+    }
+}
 
+// Call the async function to connect to the database
+connectToDatabase();
 
 app.get('/', async (request, response) => {
     try {
-        const data = await dbCollection.find().sort({ likes: -1 }).toArray();
+        const data = await collection.find().sort({ likes: -1 }).toArray();
         response.render('index.ejs', { info: data });
     } catch (error) {
         console.error(error);
     }
 });
 
-app.post('/addAlbum', (request, response) => {
-    dbCollection.insertOne({
-    coverImage : request.body.coverImage,
-    artist: request.body.artist,
-    title: request.body.title, likes: 0})
-    .then(result => {
-        console.log('Rapper Added')
-        response.redirect('/')
-    })
-    .catch(error => console.error(error))
-})
+app.post('/addAlbum', async (request, response) => {
+    try {
+        const { coverImage, artist, title } = request.body;
+        const result = await collection.insertOne({ coverImage, artist, title, likes: 0 });
+        console.log('Rapper Added');
+        response.redirect('/');
+    } catch (error) {
+        console.error(error);
+    }
+});
 
-app.put('/addOneLike', (request, response) => {
-    dbCollection.updateOne({
-        coverImage : request.body.coverImageS, 
-        artist: request.body.artistS, 
-        title: request.body.titleS,
-        likes: request.body.likesS},{
-        $set: {
-            likes:request.body.likesS + 1
-        }
-    },{
-        sort: {_id: -1},
-        upsert: false
-    })
-    .then(result => {
-        console.log('Added One Like')
-        response.json('Like Added')
-        // response.redirect('/')
-    })
-    .catch(error => console.error(error))
-})
+app.put('/addOneLike', async (request, response) => {
+    try {
+        const { coverImageS, artistS, titleS, likesS } = request.body;
+        const result = await collection.updateOne(
+            {
+                coverImage: coverImageS,
+                artist: artistS,
+                title: titleS,
+                likes: likesS
+            },
+            {
+                $set: { likes: likesS + 1 }
+            }
+        );
+        console.log('Added One Like');
+        response.json('Like Added');
+    } catch (error) {
+        console.error(error);
+    }
+});
 
-app.delete('/deleteAlbum', (request, response) => {
-    dbCollection.deleteOne({artist: request.body.artistS})
-    .then(result => {
-        console.log('Album Deleted')
-        response.json('Album Deleted')
-    })
-    .catch(error => console.error(error))
+app.delete('/deleteAlbum', async (request, response) => {
+    try {
+        const { artistS } = request.body;
+        const result = await collection.deleteOne({ artist: artistS });
+        console.log('Album Deleted');
+        response.json('Album Deleted');
+    } catch (error) {
+        console.error(error);
+    }
+});
 
-})
-
-app.listen(process.env.PORT || PORT, ()=>{
+app.listen(process.env.PORT || PORT, () => {
     console.log(`Listening on port http://localhost:${PORT}`);
-})
+});
